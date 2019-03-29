a0dcde44f4435888ca3597c834ce59355f3ab872
alexandre2r <alex.rodren@gmail.com>
2019-03-29 19:10:14 +0100

    Modificated to use new tracker config in yml

--- dl_objecttracker/Tracker/tracker.py
+++ dl_objecttracker/Tracker/tracker.py
@@ -1,11 +1,20 @@
-import cv2
+import os
 import time
+import yaml
+import cv2
 import dlib
 
-class Tracker:
-    def __init__(self):
 
-        self.tracker = cv2.MultiTracker_create()
+class Tracker:
+    def __init__(self, tracker_prop, tracker_lib):
+
+        if tracker_lib == 'opencv':
+            self.tracker = cv2.MultiTracker_create()
+            self.lib = 'opencv'
+            self.type = tracker_prop['Type']
+        elif tracker_lib == 'dlib':
+            self.lib = 'dlib'
+            self.type = ''
         print("Tracker created!")
         self.activated = False
         self.input_detection = None
@@ -32,6 +41,7 @@ class Tracker:
         self.frame_tags = []
         self.log_data = []
         self.log_tracking_results = []
+        self.log_done = False
 
     def imageToTrack(self):
         ''' Assigns a new image to track depending on certain conditions. '''
@@ -75,41 +85,51 @@ class Tracker:
             self.counter_fast += 1
             self.tracker_fast = True
 
-    def configureFirstTrack(self, detection):
+    def configureFirstTrack(self, detection, type):
         ''' Configures the tracker with the detections from the net. '''
 
-        #dlib
-        # if self.network_framework == "Keras":
-        #     xmin = detection[1]
-        #     ymin = detection[0]
-        #     xmax = detection[3]
-        #     ymax = detection[2]
-        # elif self.network_framework == "TensorFlow":
-        #     xmin = detection[0]
-        #     ymin = detection[1]
-        #     xmax = detection[2]
-        #     ymax = detection[3]
-        #
-        # t = dlib.correlation_tracker()
-        # rect = dlib.rectangle(xmin, ymin, xmax, ymax)
-        # if not rect.is_empty():
-        #     t.start_track(self.image, rect)
-        #     self.trackers_dlib.append(t)
+        if self.lib == 'dlib':
+            if self.network_framework == "Keras":
+                xmin = detection[1]
+                ymin = detection[0]
+                xmax = detection[3]
+                ymax = detection[2]
+            elif self.network_framework == "TensorFlow":
+                xmin = detection[0]
+                ymin = detection[1]
+                xmax = detection[2]
+                ymax = detection[3]
 
-        #opencv
-        if self.network_framework == "Keras":
-            xmin = detection[1]
-            ymin = detection[0]
-            xmax = detection[3] - xmin
-            ymax = detection[2] - ymin
-        elif self.network_framework == "TensorFlow":
-            xmin = detection[0]
-            ymin = detection[1]
-            xmax = detection[2] - xmin
-            ymax = detection[3] - ymin
+            t = dlib.correlation_tracker()
+            rect = dlib.rectangle(xmin, ymin, xmax, ymax)
+            if not rect.is_empty():
+                t.start_track(self.image, rect)
+                self.trackers_dlib.append(t)
 
-        self.tracker.add(cv2.TrackerMOSSE_create(), self.image, (
-            xmin, ymin, xmax, ymax))
+        else:
+            if self.network_framework == "Keras":
+                xmin = detection[1]
+                ymin = detection[0]
+                xmax = detection[3] - xmin
+                ymax = detection[2] - ymin
+            elif self.network_framework == "TensorFlow":
+                xmin = detection[0]
+                ymin = detection[1]
+                xmax = detection[2] - xmin
+                ymax = detection[3] - ymin
+
+            OPENCV_OBJECT_TRACKERS = {
+                "kcf": cv2.TrackerKCF_create,
+                "boosting": cv2.TrackerBoosting_create,
+                "mil": cv2.TrackerMIL_create,
+                "tld": cv2.TrackerTLD_create,
+                "medianflow": cv2.TrackerMedianFlow_create,
+                "csrt": cv2.TrackerCSRT_create,
+                "mosse": cv2.TrackerMOSSE_create
+            }
+            tracker = OPENCV_OBJECT_TRACKERS[type.lower()]()
+            self.tracker.add(tracker, self.image, (
+                xmin, ymin, xmax, ymax))
 
     def track(self):
         ''' The tracking function. '''
@@ -118,7 +138,8 @@ class Tracker:
 
         if detection is not None and self.new_detection:  # new detection from net
 
-            self.tracker = cv2.MultiTracker_create()
+            if self.lib == 'opencv':
+                self.tracker = cv2.MultiTracker_create()
             self.last_fps_buffer = [0 for _ in range(3)]
             self.first_image_to_track = True
 
@@ -134,51 +155,58 @@ class Tracker:
 
             if self.activated:  # avoid to continue the loop if not activated
 
-                # if self.new_detection: #dlib
-                #     self.trackers_dlib = []
-                #     self.labels_dlib = []
+                if self.lib == 'dlib':
+                    if self.new_detection:
+                        self.trackers_dlib = []
+                        self.labels_dlib = []
                 for i in range(len(detection)):
                     if self.first_image_to_track:  # create multitracker only in the first frame of the buffer
-                        self.configureFirstTrack(detection[i])
-                        # self.labels_dlib = self.input_label #dlib
+                        self.configureFirstTrack(detection[i], self.type)
+                        if self.lib == 'dlib':
+                            self.labels_dlib = self.input_label
 
                 self.first_image_to_track = False
                 self.new_detection = False
-                _, boxes = self.tracker.update(self.image)
 
-                for i, newbox in enumerate(boxes):
-                    p1 = (int(newbox[0]), int(newbox[1]))
-                    p2 = (int(newbox[0] + newbox[2]), int(newbox[1] + newbox[3]))
-                    if len(self.input_label) == len(boxes):
+                if self.lib == 'opencv':
+                    _, boxes = self.tracker.update(self.image)
+
+                    for i, newbox in enumerate(boxes):
+                        p1 = (int(newbox[0]), int(newbox[1]))
+                        p2 = (int(newbox[0] + newbox[2]), int(newbox[1] + newbox[3]))
+                        if len(self.input_label) == len(boxes):
+                            cv2.rectangle(self.image, p1, p2, self.color_list[self.input_label[i]], thickness=2)
+                            cv2.putText(self.image, self.input_label[i], (p1[0], p1[1] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                                        0.45,
+                                        (0, 0, 0), thickness=2, lineType=2)
+                            cv2.putText(self.image, 'FPS avg tracking: ' + str(int(self.avg_fps)), (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
+                                        0.45,
+                                        (255, 0, 0), thickness=1, lineType=1)
+                            # log
+                            self.log_tracking_results.append([self.frame_tags[0], self.input_label[i], p1, p2])
+
+                            # print('Tracker in: ' + str(self.frame_tags[0]))
+                else:
+                    for i, (t, l) in enumerate(zip(self.trackers_dlib, self.labels_dlib)):
+                        # update the tracker and grab the position of the tracked object
+                        t.update(self.image)
+                        pos = t.get_position()
+
+                        # unpack the position object
+                        p1 = (int(pos.left()), int(pos.top()))
+                        p2 = (int(pos.right()), int(pos.bottom()))
+
+                        # draw the bounding box from the dlib tracker
                         cv2.rectangle(self.image, p1, p2, self.color_list[self.input_label[i]], thickness=2)
-                        cv2.putText(self.image, self.input_label[i], (p1[0], p1[1] - 10), cv2.FONT_HERSHEY_SIMPLEX,
+                        cv2.putText(self.image, l, (p1[0], p1[1] - 10), cv2.FONT_HERSHEY_SIMPLEX,
                                     0.45,
                                     (0, 0, 0), thickness=2, lineType=2)
-                        cv2.putText(self.image, 'FPS avg tracking: ' + str(int(self.avg_fps)), (10, 20), cv2.FONT_HERSHEY_SIMPLEX,
+                        cv2.putText(self.image, 'FPS avg tracking: ' + str(int(self.avg_fps)), (10, 20),
+                                    cv2.FONT_HERSHEY_SIMPLEX,
                                     0.45,
                                     (255, 0, 0), thickness=1, lineType=1)
-                        self.log_tracking_results.append([self.frame_tags[0], self.input_label[i], p1, p2])
-
-                        # print('Tracker in: ' + str(self.frame_tags[0]))
-
-                # for i, (t, l) in enumerate(zip(self.trackers_dlib, self.labels_dlib)): #dlib
-                #     # update the tracker and grab the position of the tracked object
-                #     t.update(self.image)
-                #     pos = t.get_position()
-                #
-                #     # unpack the position object
-                #     p1 = (int(pos.left()), int(pos.top()))
-                #     p2 = (int(pos.right()), int(pos.bottom()))
-                #
-                #     # draw the bounding box from the dlib tracker
-                #     cv2.rectangle(self.image, p1, p2, self.color_list[self.input_label[i]], thickness=2)
-                #     cv2.putText(self.image, l, (p1[0], p1[1] - 10), cv2.FONT_HERSHEY_SIMPLEX,
-                #                 0.45,
-                #                 (0, 0, 0), thickness=2, lineType=2)
-                #     cv2.putText(self.image, 'FPS avg tracking: ' + str(self.avg_fps)[:5], (10, 20),
-                #                 cv2.FONT_HERSHEY_SIMPLEX,
-                #                 0.45,
-                #                 (255, 0, 0), thickness=1, lineType=1)
+                        # log
+                        self.log_tracking_results.append([self.frame_tags[0], l, p1, p2])
 
                 self.buffer_out.append(self.image)
                 avg_fps = self.calculateFPS(start_time)
@@ -215,7 +243,6 @@ class Tracker:
             self.image_counter += 1
             if len(self.frame_tags) > 0:
                 frame_tag = self.frame_tags.pop(0)
-                # print(frame_tag)
                 self.log_data.append(frame_tag)
             return self.buffer_out.pop(0)  # returns last detection and deletes it
         elif self.buffer_out and self.tracker_slow:
@@ -223,14 +250,12 @@ class Tracker:
                 self.image_counter += 3
                 if len(self.frame_tags) > 0:
                     frame_tag = self.frame_tags.pop(0)
-                    # print(frame_tag)
                     self.log_data.append(frame_tag)
                 return self.buffer_out.pop(0)
             else:
                 self.image_counter += 1
                 if len(self.frame_tags) > 0:
                     frame_tag = self.frame_tags.pop(0)
-                    # print(frame_tag)
                     self.log_data.append(frame_tag)
                 return self.buffer_out.pop(0)
         elif self.buffer_out and self.tracker_fast and self.counter_fast == 1:  # slow tracking a little, wait for network result
@@ -239,14 +264,12 @@ class Tracker:
             self.tracker_fast = False
             if len(self.frame_tags) > 0:
                 frame_tag = self.frame_tags.pop(0)
-                # print(frame_tag)
                 self.log_data.append(frame_tag)
             return self.buffer_out.pop(0)
         elif self.buffer_out and self.input_detection is None:
             self.image_counter += 1
             if len(self.frame_tags) > 0:
                 frame_tag = self.frame_tags.pop(0)
-                # print(frame_tag)
                 self.log_data.append(frame_tag)
             return self.buffer_out.pop(0)
         else:
@@ -264,6 +287,14 @@ class Tracker:
             self.counter_fast = 0
             self.new_detection = False
             print('Tracking done!')
+
+    def logTracking(self):
+        if os.path.isfile('log_tracking.yaml') and not self.log_done:
+            with open('log_tracking.yaml', 'w') as yamlfile:
+                print(self.log_tracking_results)
+                yaml.safe_dump(self.log_tracking_results, yamlfile, explicit_start=True, default_flow_style=False)
+            self.log_done = True
+            print('Log tracker done!')
 
     def toggleTracker(self):
         ''' Toggles the tracker (on/off). '''
