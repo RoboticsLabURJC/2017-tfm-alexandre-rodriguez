d5ce02ed713ebf5bacfac261d219c71c7d68d4a0
alexandre2r <alex.rodren@gmail.com>
2019-05-04 17:30:52 +0200

    Added MOT parsing, ignoring ids, hardcoded class

--- dl_objecttracker/groundtruths/convert_gt_to_pascalvoc.py
+++ dl_objecttracker/groundtruths/convert_gt_to_pascalvoc.py
@@ -1,16 +1,21 @@
-import yaml
 from os import path
 
-#gt format required: <class_name> <left> <top> <right> <bottom>
+# groundtruth format required: <class_name> <left> <top> <right> <bottom>
 
 
 # ToDo: modify according to dataset gt format
 def get_files(dataset_name, path_file, previous_frame, target):
-    if dataset_name == 'mot':  # ToDo: include ids in tracking (pending)
+    if dataset_name == 'mot':  # ToDo: include ids in tracking (pending); now not taking id into account
+        folder_path = path_file.rsplit('/', 1)[0]
         with open(path_file, 'r') as stream:
             out = stream.readlines()  # <frame>, <id>, <bb_left>, <bb_top>, <bb_width>, <bb_height>, <conf>, <x>, <y>, <z>
             for i in range(len(out)):
-                print(out[i])
+                out[i] = out[i].split(',')
+                with open(folder_path + '/' + str(int(out[i][0]) - 1) + '.txt', 'a') as logfile:
+                    logfile.write(
+                        target + " " + str(out[i][2]) + " " + str(out[i][3]) + " " + str(int(
+                            out[i][2]) + int(out[i][4])) + " " + str(int(out[i][3]) + int(out[i][5])) + "\n")
+
     elif dataset_name == 'otb':
         folder_path = path_file.rsplit('/', 1)[0]
         with open(path_file, 'r') as stream:
@@ -28,16 +33,16 @@ def get_files(dataset_name, path_file, previous_frame, target):
             for i in range(len(out)):
                 out[i] = out[i].split(' ')
                 with open(folder_path + '/' + str(i) + '.txt', 'w') as logfile:
-                    logfile.write(target + " " + str(out[i][1]) + " " + str(out[i][2]) + " " + str(int(
-                            out[i][1])+int(out[i][3])) + " " + str(int(out[i][2])+int(out[i][4])) + "\n")
+                    logfile.write(target + " " + str(out[i][1]) + " " + str(out[i][2]) + " " + str(int(out[i][3])) + " " + str(int(out[i][4])) + "\n")
 
 
 if __name__ == "__main__":
     file_path = None
     target = None
-    dataset = 'nfs'
+    dataset = 'mot'
     if dataset == 'mot':
-        file_path = path.relpath("/media/alexandre/Data/Documents/Alexandre2R/MOVA/TFM/video/MOT17Det/MOT17DetLabels/train/MOT17-11/gt/gt.txt")
+        file_path = path.relpath("/media/alexandre/Data/Documents/Alexandre2R/MOVA/TFM/video/MOT17Det/MOT17DetLabels/train/MOT17-10/gt/gt.txt")
+        target = 'person'
     elif dataset == 'otb':
         file_path = path.relpath("/media/alexandre/Data/Documents/Alexandre2R/MOVA/TFM/video/OTB100/Basketball/groundtruth_rect.txt")
         target = 'person'  # hardcoded class label
