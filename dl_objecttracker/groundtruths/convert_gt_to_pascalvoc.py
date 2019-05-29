253e96f16fc6f1cdf16e8e81978d8a76e7232492
alexandre2r <alex.rodren@gmail.com>
2019-05-29 22:30:41 +0200

    Changed: otb and nfs not useful

--- dl_objecttracker/groundtruths/convert_gt_to_pascalvoc.py
+++ dl_objecttracker/groundtruths/convert_gt_to_pascalvoc.py
@@ -2,10 +2,9 @@ from os import path
 
 
 # groundtruth format required: <class_name> <left> <top> <right> <bottom>
+# https://github.com/rafaelpadilla/Object-Detection-Metrics#create-your-detection-files
 
-
-# ToDo: modify according to dataset gt format
-def get_files(dataset_name, path_file, previous_frame, target):
+def get_files(dataset_name, path_file):
     if dataset_name == 'mot':  # ToDo: include ids in tracking (pending); now not taking id into account
         folder_path = path_file.rsplit('/', 1)[0]
         label_classes_dict = {'1': 'person',
@@ -31,42 +30,41 @@ def get_files(dataset_name, path_file, previous_frame, target):
                             label + " " + str(out[i][2]) + " " + str(out[i][3]) + " " + str(int(
                                 out[i][2]) + int(out[i][4])) + " " + str(int(out[i][3]) + int(out[i][5])) + "\n")
 
-    elif dataset_name == 'otb':
-        folder_path = path_file.rsplit('/', 1)[0]
-        with open(path_file, 'r') as stream:
-            out = stream.readlines()  # (x, y, box-width, box-height)
-            for i in range(len(out)):
-                out[i] = out[i].split(',')
-                with open(folder_path + '/' + str(i) + '.txt', 'a') as logfile:
-                    logfile.write(
-                        target + " " + str(out[i][0]) + " " + str(out[i][1]) + " " + str(int(
-                            out[i][0]) + int(out[i][2])) + " " + str(int(out[i][1]) + int(out[i][3][:2])) + "\n")
-
-    elif dataset_name == 'nfs':
-        folder_path = path_file.rsplit('/', 1)[0]
-        with open(path_file, 'r') as stream:
-            out = stream.readlines()  # (*, left, top, right, bottom, frame, *, *, *, class)
-            for i in range(len(out)):
-                out[i] = out[i].split(' ')
-                with open(folder_path + '/' + str(i) + '.txt', 'w') as logfile:
-                    logfile.write(
-                        target + " " + str(out[i][1]) + " " + str(out[i][2]) + " " + str(int(out[i][3])) + " " + str(
-                            int(out[i][4])) + "\n")
+    # elif dataset_name == 'otb':
+    #     folder_path = path_file.rsplit('/', 1)[0]
+    #     with open(path_file, 'r') as stream:
+    #         out = stream.readlines()  # (x, y, box-width, box-height)
+    #         for i in range(len(out)):
+    #             out[i] = out[i].split(',')
+    #             with open(folder_path + '/' + str(i) + '.txt', 'a') as logfile:
+    #                 logfile.write(
+    #                     target + " " + str(out[i][0]) + " " + str(out[i][1]) + " " + str(int(
+    #                         out[i][0]) + int(out[i][2])) + " " + str(int(out[i][1]) + int(out[i][3][:2])) + "\n")
+    #
+    # elif dataset_name == 'nfs':
+    #     folder_path = path_file.rsplit('/', 1)[0]
+    #     with open(path_file, 'r') as stream:
+    #         out = stream.readlines()  # (*, left, top, right, bottom, frame, *, *, *, class)
+    #         for i in range(len(out)):
+    #             out[i] = out[i].split(' ')
+    #             with open(folder_path + '/' + str(i) + '.txt', 'w') as logfile:
+    #                 logfile.write(
+    #                     target + " " + str(out[i][1]) + " " + str(out[i][2]) + " " + str(int(out[i][3])) + " " + str(
+    #                         int(out[i][4])) + "\n")
 
 
 if __name__ == "__main__":
     file_path = None
-    target = None
     dataset = 'mot'
     if dataset == 'mot':
         file_path = path.relpath(
-            "/media/alexandre/Data/Documents/Alexandre2R/MOVA/TFM/video/MOT17Det/MOT17DetLabels/train/MOT17-11/gt/gt.txt")
-    elif dataset == 'otb':
-        file_path = path.relpath(
-            "/media/alexandre/Data/Documents/Alexandre2R/MOVA/TFM/video/OTB100/Basketball/groundtruth_rect.txt")
-        target = 'person'  # hardcoded class label
-    elif dataset == 'nfs':
-        file_path = path.relpath("/media/alexandre/Data/Documents/Alexandre2R/MOVA/TFM/video/NFS/airboard_1.txt")
-        target = 'class'
-    previous_frame_number = None
-    get_files(dataset, file_path, previous_frame_number, target)
+            "/media/alexandre/Data/Documents/Alexandre2R/MOVA/TFM/video/MOT17Det/MOT17DetLabels/train/MOT17-13/gt/gt.txt")
+        #ToDo: move gt.txt from gt folder automatically, input file by args
+    # elif dataset == 'otb':
+    #     file_path = path.relpath(
+    #         "/media/alexandre/Data/Documents/Alexandre2R/MOVA/TFM/video/OTB100/Basketball/groundtruth_rect.txt")
+    #     target = 'person'  # hardcoded class label
+    # elif dataset == 'nfs':
+    #     file_path = path.relpath("/media/alexandre/Data/Documents/Alexandre2R/MOVA/TFM/video/NFS/airboard_1.txt")
+    #     target = 'class'
+    get_files(dataset, file_path)
